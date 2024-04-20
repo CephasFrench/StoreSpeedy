8ec550dd6fad40de51a72c3c882818eb40e80bad
french23 <peterfstewart@outlook.com>
2024-04-20 11:01:23 -0500

    stuff

--- StoreSpeedy/App.js
+++ StoreSpeedy/App.js
@@ -1,8 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import {useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios, {Axios} from "axios";
 
 export default function App() {
+
+  useEffect(() => {
+    testFunc();
+    console.log("hello in app");
+  }, []);
+  const testFunc = () => {
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8080/test/testFunc',
+      headers: { }
+    };
+
+    axios.request(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
